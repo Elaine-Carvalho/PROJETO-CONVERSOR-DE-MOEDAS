858afd09c9d7acceb6c55037738c85146765063e
Elaine-carvalho <elainecarvalhob@hotmail.com>
2023-10-11 15:45:21 -0300

    analisar js

---
+++ script.js
@@ -0,0 +1,11 @@
+const convertButton = document.querySelector(".convert-button")
+
+function convertValues(){
+    console.log("oi")
+}
+convertButton.addEventListener("click", convertValues)
+
+
+
+
+
